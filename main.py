0395531d3d048c617ca3ff692fbb9a80f4b32d00
PhuCuong Phung <cuongphungphu@gmail.com>
2023-05-18 16:58:17 +0700

    add image

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import cv2 as cv
+import cv2
+import numpy as np
+
+# Load the image
+image = cv2.imread('Chessboard_0481_1.png')
+
+# Convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Image', gray)
+cv2.waitKey(0)
